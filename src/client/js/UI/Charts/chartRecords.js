3c3caaa49b79e475bdfd5ef2215b62257d2299fa
Amro Abdelkamel <amromoorie@gmail.com>
2022-08-04 23:42:41 +0200

    Feat: chart records + bring it all together

--- src/client/js/UI/Charts/chartRecords.js
+++ src/client/js/UI/Charts/chartRecords.js
@@ -9,6 +9,7 @@ import {
   PointElement,
   Title,
 } from 'chart.js'
+import { generateGlobalRecords } from './globalRecords'
 Chartify.register(
   LineController,
   LineElement,
@@ -21,24 +22,57 @@ Chartify.register(
 )
 
 export default class Chart {
-  // table element
-  constructor(data) {
+
+  constructor(data, years) {
     this.ctx = document.getElementById('chart')
-    this.data = Object.values(data)
+    this.xLabel = Object.values(data)
     this.records = []
+    this.data = data
+    this.years = years
+    this.d = [{Year: "1906", Glob: "-0.23", NHem: -0.22, SHem: -0.23},{Year: "1909", Glob: "-0.47", NHem: -0.46, SHem: -0.49},{Year: "1910", Glob: "-0.43", NHem: -0.43, SHem: -0.44},
+  {Year: "1908", Glob: "-0.43", NHem: -0.45, SHem: -0.42},{Year: "1911", Glob: "-0.43", NHem: -0.39, SHem: -0.47}]
   }
 
- 
+
+ generateDataset() {
+console.log('data',this.data)
+ }
 
   generateChart() {
     new Chartify(this.ctx, {
-      type: 'bar',
+      type: 'line',
       data: {
-        labels: [],
+        // labels: [],
         datasets: [
-          
-        ],
+          {
+              label: 'Global World Temperature',
+              data: this.data,
+                tension: 0.4,
+      parsing: {
+        xAxisKey: 'Year',
+        yAxisKey: 'Glob'
       },
+              // backgroundColor: [
+              //   'rgba(255, 99, 132, 0.2)',
+              //   'rgba(54, 162, 235, 0.2)',
+              //   'rgba(255, 206, 86, 0.2)',
+              //   'rgba(75, 192, 192, 0.2)',
+              //   'rgba(153, 102, 255, 0.2)',
+              //   'rgba(255, 159, 64, 0.2)',
+              // ],
+              // borderColor: [
+              //   'rgba(255, 99, 132, 1)',
+              //   'rgba(54, 162, 235, 1)',
+              //   'rgba(255, 206, 86, 1)',
+              //   'rgba(75, 192, 192, 1)',
+              //   'rgba(153, 102, 255, 1)',
+              //   'rgba(255, 159, 64, 1)',
+              // ],
+              borderWidth: 1,
+            },
+      ],
+    
+    },
     })
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-// import { view } from './js/view';
-import { weatherData } from './client/js/weatherInfo';
-import "./client/style.scss"
-import "./client/mediaQuery.scss"
-import hello from './client/js/view';
+import Chart from './client/js/UI/Charts/chartRecords'
+import Table from './client/js/UI/tabulateRecords'
+import { weatherData } from './client/js/weatherInfo'
+import './client/styles/style.scss'
 
-weatherData();
+const { head, data } = weatherData()
+const chart = new Chart(data)
+chart.generateChart()
+chart.generateDataset()
 
+const tabulate = new Table(head, data)
+tabulate.generateTableData()
 
-// export { weatherData };
+export const handleFetchMoreRecords = () => {
+  const btn = document.querySelector('button')
+  btn.addEventListener('click', tabulate.displayMoreRecords())
+}
